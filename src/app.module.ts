1a1c800d2f79b6052d3a87f03a68f50eb5f42adf
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-06-23 23:50:31 -0500

    connect with typeorm postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-// import { UsersModule } from './users/users.module';
 import { MembershipsModule } from './memberships/memberships.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+ 
 
 @Module({
-  imports: [UsersModule, MembershipsModule],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+    }), // Global configuration module
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService]
+    }),
+    UsersModule, MembershipsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,10 +1,18 @@
+import type { ConfigService } from "@nestjs/config";
 import type { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { join } from 'path';
 
 
 
-
-export const typeOrmConfig = (): TypeOrmModuleOptions => ({
+export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'postgres',
-    
- 
+    host: configService.get('DATABASE_HOST'),
+    port: configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USER'),
+    password: configService.get('DATABASE_PASS'),
+    database: configService.get('DATABASE_NAME'),
+    ssl: true,
+    logging: false,
+    entities: [join(__dirname + '../../**/*.entity.{js,ts}')],
+    synchronize: true,
 });
